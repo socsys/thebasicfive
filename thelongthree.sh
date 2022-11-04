4b48c9eaee944a2299adddb52c291903addbc6b0
MMMKassem <mkassem.44t@gmail.com>
2022-11-04 09:58:00 -0500

    1 second interval

--- thelongthree.sh
+++ thelongthree.sh
@@ -4,10 +4,11 @@ sudoPW="snuc"
 
 server="35.242.187.72"		#Google Server London
 
-ping_duration=2
-hping_duration=2
+ping_duration=1
+hping_duration=1
 hping_port=4040
-tracrt_cnt=2
+tracrt_cnt=1
+iteration=1
 
 current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
 dirname="results".$current_time
@@ -15,19 +16,20 @@ mkdir $dirname
 
 while :
 do
-	current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
-	echo "1. Start ping at Current Time : $current_time"
+	current_time1=$(date -u "+%Y.%m.%d-%H.%M.%S")
+	echo "--------------------->"$iteration."at ".$current_time1
+	echo "1. Start ping "
 	ping -c $ping_duration $server | while read pong; do echo "$(date -u): $pong" >> ./$dirname/"ping_"$current_time".log" ; done
 
-	current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
-	echo "2. Start hping at Current Time : $current_time"
-	echo $sudoPW | sudo -S hping3 -S $server -p $hping_port -c $hping_duration >> ./$dirname/"hping_"$current_time".log"
+	#current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
+	echo "2. Start hping"
+	echo $sudoPW | sudo -S hping3 -S $server -p $hping_port -c $hping_duration >> ./$dirname/"hping_"$current_time".log" 2>&1
 
 	tracrt_cnt=2
-	current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
-	echo "3. Start Traceroute at Current Time : $current_time"
+	#current_time=$(date -u "+%Y.%m.%d-%H.%M.%S")
+	echo "3. Start Traceroute "
 	while [ $tracrt_cnt -gt 0 ]; do traceroute $server >> ./$dirname/"traceroute_"$current_time".log"; ((tracrt_cnt=tracrt_cnt-1)); done
 
 	sleep 1
-	echo "--------------------->"
+	((iteration=iteration+1))
 done
